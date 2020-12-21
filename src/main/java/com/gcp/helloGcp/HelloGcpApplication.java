cd37ca66a79a311c525f719816a25042447919b2
sandipbhot90@gmail.com <sandipbhot90@gmail.com>
2020-12-21 13:52:54 +0530

    hellpGcp

---
+++ src/main/java/com/gcp/helloGcp/HelloGcpApplication.java
@@ -0,0 +1,13 @@
+package com.gcp.helloGcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGcpApplication.class, args);
+	}
+
+}
